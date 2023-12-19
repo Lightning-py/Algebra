eae0c941196f83989cd1da12bc077dae3ae4f6a6
Sergey <sergejananev78@gmail.com>
2023-12-19 15:49:00 +0300

    forgot to add main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>  // std::{cout, endl}
+#include <stdexcept> // std::{invalid_argument, out_of_range}
+
+#include "parsing.hpp"
+#include "interpreter.hpp"
+#include "variables.hpp"
+
+int main()
+{
+  try
+  {
+    arch::Interpreter inpr("../log.txt");
+    inpr.readCommand();
+    std::cout << "shreck\n";
+  }
+  catch (const std::invalid_argument &exc)
+  {
+    std::cout << exc.what() << std::endl;
+  }
+  catch (const std::out_of_range &exc)
+  {
+    std::cout << exc.what() << std::endl;
+  }
+
+  system("pause");
+  return 0;
+}
